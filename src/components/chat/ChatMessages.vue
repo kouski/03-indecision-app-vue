512567cec1353f2e7137a148d5860d73a465aa13
kouski <72550619+kouski@users.noreply.github.com>
2025-03-10 12:53:25 +0100

    Componente ChatBubble

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,20 +1,14 @@
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+
+</script>
+
 <template>
   <div class="flex-1 overflow-y-auto p-4">
       <div class="flex flex-col space-y-2">
         <!-- Messages go here -->
-        <!-- Example Message -->
-        <div class="flex justify-end">
-          <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Hey, how's your day going?
-          </div>
-        </div>
-
-        <!-- Example Received Message -->
-        <div class="flex">
-          <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Not too bad, just a bit busy. How about you?
-          </div>
-        </div>
+      <ChatBubble :its-mine= "true" :message="'Hola Kouski'"/>
+      <ChatBubble :its-mine= "false" :message="'Yes'" image="https://yesno.wtf/assets/yes/13-c3082a998e7758be8e582276f35d1336.gif"/>
       </div>
     </div>
 </template>
